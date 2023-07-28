388ef4dfd9a90ab273ecfa3cd944e0433dcae863
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-07-28 15:02:33 -0300

    Update verifica_token.php

--- apoio/verifica_token.php
+++ apoio/verifica_token.php
@@ -10,17 +10,21 @@ if ($user === "") {
 if ($token === "") {
     $token = null;
 }
+
 $usuarios = verificaToken($user, $token);
 
 if (isset($usuarios['senhaCorreta'])) {
-    if ($usuarios['senhaCorreta'] == true) {
-        header('Location: autenticado.php?idUsuario=' . urlencode($usuarios['idUsuario']));
-    } else {
+    if ($usuarios['senhaCorreta'] == false) {
         $mensagem = "Token não cadastrado ou incorreto!";
         header('Location: teste_token.php?mensagem=' . urlencode($mensagem));
+    } else {
+        header('Location: autenticado.php?idUsuario=' . urlencode($usuarios['idUsuario']));
     }
 } else {
+    echo json_encode($user);
+    echo json_encode($token);
+    echo json_encode($usuarios);
     $mensagem = $usuarios['retorno'];
-    header('Location: teste_token.php?mensagem=' . urlencode($mensagem));
+    //header('Location: teste_token.php?mensagem=' . urlencode($mensagem));
 }
 ?>
